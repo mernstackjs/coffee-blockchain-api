9b9a4a95b2cae48e41b88c0a54dc4d5677e15bb7
Ahmed Abdirizaak <mernstack.js@gmail.com>
2026-08-27 15:37:51 +0200

    feat: add POST transaction endpoint

--- src/app.js
+++ src/app.js
@@ -3,8 +3,20 @@ import { Blockchain } from './blockchain.js';
 
 export const app = express();
 
+app.use(express.json());
+
 const blockchain = new Blockchain();
 
 app.get('/blockchain', (req, res) => {
   res.status(200).json(blockchain);
+});
+
+app.post('/transactions', (req, res) => {
+  const transaction = req.body;
+
+  blockchain.addTransaction(transaction);
+
+  res.status(201).json({
+    message: 'Transaction added',
+  });
 });
